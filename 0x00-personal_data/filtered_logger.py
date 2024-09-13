f616acb6b7e3de8c08d00c49cb9db0c42dca12c1
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-09-13 12:42:46 +0300

    Task1[Add]: Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,3 +12,22 @@ def filter_datum(fields: List[str], redaction: str,
                   lambda match: '{}={}'.format(match.group(1), redaction)
                   if match.group(1) in fields
                   else '{}={}'.format(match.group(1), match.group(2)), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Init function"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """format function"""
+        msg = logging.Formatter(self.FORMAT).format(record)
+        return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
