1051ce66f6f58bdf164d46e53cf06490f8dbd0f1
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-09-13 14:13:03 +0300

    Task5[Add]: Encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Encrypting passwords"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """hash_password function"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
