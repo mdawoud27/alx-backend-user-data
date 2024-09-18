a6206fdad058619d928fb9bebcc4346522bb2ce9
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-09-18 18:31:22 +0300

    Task3[Add]: Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Auth class"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth Class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """require_auth function"""
+        if not path or not excluded_paths:
+            return True
+
+        path = path if path.endswith('/') else path + '/'
+        for excluded_path in excluded_paths:
+            if (excluded_path.endswith('*')
+                    and path.startswith(excluded_path[:-1])):
+                return False
+
+            if path == excluded_path:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """authorization_header function"""
+        if not request:
+            return None
+        return request.headers.get('Authorization', None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current_user function"""
+        return None
