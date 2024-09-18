2f35f2b734017de65e00b0f67035745abd2311a7
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-09-18 18:46:29 +0300

    Task5[Add]: Request validation!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -3,6 +3,8 @@
 Route module for the API
 """
 from os import getenv
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -12,6 +14,14 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+auth = None
+auth_type = getenv("AUTH_TYPE")
+
+if auth_type == "basic_auth":
+    auth = BasicAuth()
+elif auth_type == "auth":
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -31,6 +41,27 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """forbidden handler"""
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def handle_auth() -> None:
+    """handle_auth function"""
+    if not auth:
+        return
+
+    excluded_list = ['/api/v1/status/', '/api/v1/unauthorized/',
+                     '/api/v1/forbidden/']
+    require_auth = auth.require_auth(request.path, excluded_list)
+    if not require_auth:
+        return
+
+    auth_header = auth.authorization_header(request)
+    if not auth_header:
+        abort(401)
+
+    current_user = auth.current_user(request)
+    if not current_user:
+        abort(403)
 
 
 if __name__ == "__main__":
