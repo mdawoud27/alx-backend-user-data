753214a162d62bf0dc4367068fc648aa9b71f00e
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-09-19 18:41:50 +0300

    Task0[Add]: Et moi et moi et moi!

---
+++ 0x02-Session_authentication/api/v1/app.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from os import getenv
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.views import app_views
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+auth = None
+auth_type = getenv("AUTH_TYPE")
+
+if auth_type == "basic_auth":
+    auth = BasicAuth()
+elif auth_type == "session_auth":
+    auth = SessionAuth()
+elif auth_type == "auth":
+    auth = Auth()
+elif auth_type == "session_exp_auth":
+    auth = SessionExpAuth()
+elif auth_type == "session_db_auth":
+    auth = SessionDBAuth()
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """unauthorized handler"""
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """forbidden handler"""
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def handle_auth() -> None:
+    """handle_auth function"""
+    if not auth:
+        return
+
+    excluded_list = ['/api/v1/status/', '/api/v1/unauthorized/',
+                     '/api/v1/forbidden/']
+    require_auth = auth.require_auth(request.path, excluded_list)
+    if not require_auth:
+        return
+
+    auth_header = auth.authorization_header(request)
+    if not auth_header:
+        abort(401)
+
+    current_user = auth.current_user(request)
+    if not current_user:
+        abort(403)
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
