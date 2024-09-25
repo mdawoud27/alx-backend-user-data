4389c99af59baf0c3cc6c590864bb0f1591eb56c
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-09-25 09:04:58 +0300

    Task8[Add]: Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Auth Module"""
 import bcrypt
+import uuid
 from sqlalchemy.orm.exc import NoResultFound
 from db import DB
 from user import User
@@ -8,6 +9,11 @@ from user import User
 
 def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """Generate random uuid"""
+    return str(uuid.uuid4())
 
 
 class Auth:
@@ -24,3 +30,24 @@ class Auth:
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
         raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """check if the login is valid"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode("utf-8"),
+                                  user.hashed_password)
+        except NoResultFound:
+            return False
+
+    def create_session(self, email: str) -> str:
+        """Create a session for existing users"""
+        try:
+            user = self._db.find_user_by(email=email)
+
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+
+            return session_id
+        except NoResultFound:
+            return None
