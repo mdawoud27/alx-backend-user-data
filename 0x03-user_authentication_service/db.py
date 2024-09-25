49a9c1d4b0e1928fafd405c668bfb400eb47b175
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-09-25 07:50:26 +0300

    Task1[Edit]: create user {fix output format}

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -16,7 +16,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -29,7 +29,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-
+    
     def add_user(self, email: str, hashed_password: str) -> User:
         """create a new user"""
         user = User(email=email, hashed_password=hashed_password)
